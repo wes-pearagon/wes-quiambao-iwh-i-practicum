660c689d47a26c840ed93c29108d92401a4aaf51
wes <wes@pearagon.com>
2024-07-30 19:15:54 +0800

    Add link to update page

--- index.js
+++ index.js
@@ -23,20 +23,41 @@ app.get("/", async (req, res) => {
   try {
     const resp = await axios.get(games, { headers });
     const data = resp.data.results;
-    res.render("homepage", { title: "Homepage Title", data });
+    res.render("homepage", { data });
   } catch (error) {
     console.error(error);
   }
 });
-// * Code for Route 1 goes here
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get("/update-cobj", async (req, res) => {
+  res.render("updates");
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+app.post("/update-cobj", async (req, res) => {
+  const update = {
+    properties: {
+      favorite_book: req.body.newVal,
+    },
+  };
+
+  const email = req.query.email;
+  const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    "Content-Type": "application/json",
+  };
+
+  try {
+    await axios.patch(updateContact, update, { headers });
+    res.redirect("back");
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
