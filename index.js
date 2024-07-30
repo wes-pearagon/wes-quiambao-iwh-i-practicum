ab55e59d7764a58e2dd3036d665be02113e6945b
wes <wes@pearagon.com>
2024-07-30 18:48:06 +0800

    finish app.get homepage

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-const express = require('express');
-const axios = require('axios');
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
 const app = express();
 
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "pug");
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS_TOKEN = process.env["PRIVATE_APP_ACCESS_TOKEN"];
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
+app.get("/", async (req, res) => {
+  const games = "https://api.hubapi.com/crm/v3/objects/games/?properties=game_name, genre, main_character";
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS_TOKEN}`,
+    "Content-Type": "application/json",
+  };
+
+  try {
+    const resp = await axios.get(games, { headers });
+    const data = resp.data.results;
+    res.render("homepage", { title: "Homepage Title", data });
+  } catch (error) {
+    console.error(error);
+  }
+});
 // * Code for Route 1 goes here
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
@@ -66,6 +82,5 @@ app.post('/update', async (req, res) => {
 });
 */
 
-
 // * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.listen(3000, () => console.log("Listening on http://localhost:3000"));
